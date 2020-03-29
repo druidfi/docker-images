e7bc0cd88fe473f1c6848a68bca91c083637d294
Marko Korhonen <marko.korhonen@druid.fi>
2020-03-29 11:16:29 +0300

    saml-idp image

---
+++ saml-idp/files/var/www/simplesamlphp/config/authsources.php
@@ -0,0 +1,23 @@
+<?php
+
+$config = [
+
+    'admin' => [
+        'core:AdminPassword',
+    ],
+
+    'example-userpass' => [
+        'exampleauth:UserPass',
+        'user1:user1pass' => [
+            'uid' => ['1'],
+            'eduPersonAffiliation' => ['group1'],
+            'email' => 'user1@example.com',
+        ],
+        'user2:user2pass' => [
+            'uid' => ['2'],
+            'eduPersonAffiliation' => ['group2'],
+            'email' => 'user2@example.com',
+        ],
+    ],
+
+];
