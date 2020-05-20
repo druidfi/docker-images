d9037124de00072fcdb6887c5e077e2022a3ae7b
Marko Korhonen <marko.korhonen@druid.fi>
2020-05-20 15:35:05 +0300

    Support 2 paths for mounted ssh agent for now

--- drupal/base/files/entrypoints/10-ssh-agent.sh
+++ drupal/base/files/entrypoints/10-ssh-agent.sh
@@ -3,7 +3,11 @@ set -e
 
 # Test if ssh-agents are mounted and symlink them as our known ssh-auth-sock file.
 # This will only be used in local development
-if [ -S /tmp/amazeeio_ssh-agent/socket ]; then
+if [ -S /tmp/mounted_ssh-agent/socket ]; then
+  echo "Found socket from /tmp/mounted_ssh-agent/socket"
+  echo "Symlink $SSH_AUTH_SOCK to /tmp/mounted_ssh-agent/socket..."
+  ln -sf /tmp/mounted_ssh-agent/socket "$SSH_AUTH_SOCK"
+elif [ -S /tmp/amazeeio_ssh-agent/socket ]; then
   echo "Found socket from /tmp/amazeeio_ssh-agent/socket"
   echo "Symlink $SSH_AUTH_SOCK to /tmp/amazeeio_ssh-agent/socket..."
   ln -sf /tmp/amazeeio_ssh-agent/socket "$SSH_AUTH_SOCK"
