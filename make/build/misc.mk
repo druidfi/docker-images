e32e0fcf2196499f132ddae9725409131d5b441e
Marko Korhonen <marko.korhonen@druid.fi>
2021-03-06 10:55:33 +0200

    Currently building Mailhog fails, so disabling it

--- make/build/misc.mk
+++ make/build/misc.mk
@@ -5,7 +5,7 @@
 BUILD_TARGETS += build-all-misc
 
 PHONY += build-all-misc
-build-all-misc: build-curl build-dnsmasq build-mailhog build-saml-idp build-ssh-agent build-varnish ## Build all misc images
+build-all-misc: build-curl build-dnsmasq build-saml-idp build-ssh-agent build-varnish ## Build all misc images
 
 PHONY += build-curl
 build-curl: ## Build Curl image
