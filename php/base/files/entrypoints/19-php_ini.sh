d64681860673e65aa3984298e6a14c74fc8e6c02
Marko Korhonen <marko.korhonen@druid.fi>
2021-03-06 10:38:05 +0200

    Check for template to exist (e.g. on restarts it does not exist anymore)

--- php/base/files/entrypoints/19-php_ini.sh
+++ php/base/files/entrypoints/19-php_ini.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-echo "Prepare PHP 99_custom.ini conf..."
+TEMPLATE=/etc/php$PHP_MAJOR_VERSION/conf.d/99_dynamic.ini.ep
+TARGET=/etc/php$PHP_MAJOR_VERSION/conf.d/99_dynamic.ini
 
-sudo --preserve-env ep -v /etc/php$PHP_MAJOR_VERSION/conf.d/99_dynamic.ini.ep
-sudo mv /etc/php$PHP_MAJOR_VERSION/conf.d/99_dynamic.ini.ep /etc/php$PHP_MAJOR_VERSION/conf.d/99_dynamic.ini
+if [ -f "$TEMPLATE" ]; then
+  echo "Prepare PHP 99_custom.ini conf..."
+
+  sudo --preserve-env ep -v "$TEMPLATE"
+  sudo mv "$TEMPLATE" "$TARGET"
+fi
