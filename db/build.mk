d3c572e5990cfdef1689c4444c66d57025aa64d1
Marko Korhonen <marko.korhonen@druid.fi>
2021-06-11 13:18:22 +0300

    No push in GHA

--- db/build.mk
+++ db/build.mk
@@ -1,4 +1,5 @@
 BUILD_TARGETS += build-all-db
+BUILDX_PUSH := --push
 
 PHONY += build-all-db
 build-all-db: build-mariadb-10.6 build-mysql-5.7 build-mysql-8.0 ## Build all database images
@@ -6,8 +7,8 @@ build-all-db: build-mariadb-10.6 build-mysql-5.7 build-mysql-8.0 ## Build all da
 PHONY += build-mariadb-%
 build-mariadb-%: ## Build MySQL images
 	$(call step,Build druidfi/db:mariadb$*)
-	$(DBX) --target base -t druidfi/db:mariadb$*-drupal --push db/mariadb \
-		--build-arg MARIADB_VERSION=$*
+	$(DBX) --target base -t druidfi/db:mariadb$*-drupal db/mariadb \
+		--build-arg MARIADB_VERSION=$* $(BUILDX_PUSH)
 
 #
 # NOTE! No arm images available!!!
