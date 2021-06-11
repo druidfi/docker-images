9c7c77aef117ddbfc2eb5e271301a27a34076195
Marko Korhonen <marko.korhonen@druid.fi>
2021-06-11 10:52:13 +0300

    Remember always to pull

--- drupal/build.mk
+++ drupal/build.mk
@@ -1,4 +1,4 @@
 PHONY += bake-all-drupal
 bake-all-drupal: ## Bake all Drupal images (7.3, 7.4, 8.0)
-	@cd drupal && docker buildx bake --push --progress plain base-variants
-	@cd drupal && docker buildx bake --push --progress plain web-variants
+	@cd drupal && docker buildx bake --pull --push --progress plain base-variants
+	@cd drupal && docker buildx bake --pull --push --progress plain web-variants
