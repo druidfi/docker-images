7d7bdda9844b35127c4a69020f2f913f0a10028b
Marko Korhonen <marko.korhonen@druid.fi>
2021-06-28 11:24:51 +0300

    Clean up push targets

--- make/push_targets.mk
+++ make/push_targets.mk
@@ -1,9 +1,3 @@
-PHONY += push-drupal-test
-push-drupal-test: ## Push all Drupal test images to Docker Hub
-	docker push druidfi/drupal:7.3-test
-	docker push druidfi/drupal:7.4-test
-	docker push druidfi/drupal:8.0-test
-
 PHONY += push-qa
 push-qa: ## Push all QA images to Docker Hub
 	docker push druidfi/qa:php-7.3
