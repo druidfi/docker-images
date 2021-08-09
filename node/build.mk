29bab31a2dc486480c3e3588cd4be344cabf268f
Marko Korhonen <marko.korhonen@druid.fi>
2021-08-09 08:20:10 +0300

    bake-all-node description

--- node/build.mk
+++ node/build.mk
@@ -1,5 +1,5 @@
 BUILD_TARGETS += bake-all-node
 
 PHONY += bake-all-node
-bake-all-node: ## Bake all Node LTS images (8, 10, 12, 14)
+bake-all-node: ## Bake all Node LTS images (8, 10, 12, 14, 16)
 	@cd node && docker buildx bake --pull --push
