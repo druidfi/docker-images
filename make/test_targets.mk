b8d42d3d1cb2f93eee2b3db6d69ae216f0107005
Marko Korhonen <marko.korhonen@druid.fi>
2021-09-16 13:50:46 +0300

    docker.sh > docker.so

--- make/test_targets.mk
+++ make/test_targets.mk
@@ -20,19 +20,19 @@ PHONY += test-drupal-running
 test-drupal-running:
 	$(call step,Run drupal-test container)
 	@(cd tests/drupal-test/ && docker-compose down -v && docker-compose up -d --remove-orphans)
-	$(call step,See the example site: https://test-drupal.docker.sh)
+	$(call step,See the example site: https://test-drupal.docker.so)
 
 PHONY += test-wp-running
 test-wp-running:
 	$(call step,Run wp-test container)
 	@(cd tests/wp-test/ && docker-compose down -v && docker-compose up -d --remove-orphans)
-	$(call step,See the example site: https://test-wp.docker.sh)
+	$(call step,See the example site: https://test-wp.docker.so)
 
 PHONY += example-drupal
 example-drupal:
 	$(call step,Setup example Drupal setup with Nginx and PHP containers)
 	@(cd examples/drupal-separate-services && docker-compose down -v && docker-compose up -d --remove-orphans)
-	$(call step,See the example site: https://example-drupal.docker.sh)
+	$(call step,See the example site: https://example-drupal.docker.so)
 
 PHONY += test-wp
 test-wp:
