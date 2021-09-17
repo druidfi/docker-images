8eb89fb2ea0138352b2b49a5e079133369ff6c23
Marko Korhonen <marko.korhonen@druid.fi>
2021-09-17 14:42:48 +0300

    remove stuff from Drupal web which are moved already to images/php/dockerfile

--- drupal/web/files/entrypoints/20-php-fpm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "Start up PHP-FPM..."
-
-# -E option of sudo copies all env variables of current user to the root
-sudo -E LD_PRELOAD=/usr/lib/preloadable_libiconv.so php-fpm$PHP_MAJOR_VERSION
--- drupal/web/files/entrypoints/30-nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Prepare Nginx conf..."
-
-sudo --preserve-env ep -v /etc/nginx/conf.d/default.conf
-
-echo "Start up Nginx..."
-
-sudo nginx -g 'daemon off;'
