6b878699fd10c259b9be517c0fc56578f122334f
Marko Korhonen <marko.korhonen@druid.fi>
2021-09-20 10:31:32 +0300

    Remove imagemagick & change CMD for fpm and web images

--- php/files/entrypoints/20-php-fpm.sh
+++ php/files/entrypoints/20-php-fpm.sh
@@ -2,7 +2,7 @@
 
 echo "Start up PHP-FPM..."
 
-php-fpm"$PHP_MAJOR_VERSION" -D -R
+php-fpm -D -R
 
 # Usage: php [-n] [-e] [-h] [-i] [-m] [-v] [-t] [-p <prefix>] [-g <pid>] [-c <file>] [-d foo[=bar]] [-y <file>] [-D] [-F [-O]]
 #  -c <path>|<file> Look for php.ini file in this directory
--- php/files/entrypoints/30-nginx.sh
+++ php/files/entrypoints/30-nginx.sh
@@ -3,7 +3,3 @@
 echo "Prepare Nginx conf..."
 
 sudo --preserve-env ep -v /etc/nginx/conf.d/default.conf
-
-echo "Start up Nginx..."
-
-sudo nginx -g 'daemon off;'
