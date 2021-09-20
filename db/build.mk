ceaafb4e63ac74994385b1b92fa36bcfbcda842d
Marko Korhonen <marko.korhonen@druid.fi>
2021-09-20 12:06:35 +0300

    Build DB images with linux/amd64 only

--- db/build.mk
+++ db/build.mk
@@ -1,5 +1,10 @@
 BUILD_TARGETS += bake-all-db
+DB_BAKE_FLAGS := --pull --push
 
 PHONY += bake-all-db
 bake-all-db: ## Bake all Database images
-	@cd db && docker buildx bake --pull --push
+	@cd db && docker buildx bake $(DB_BAKE_FLAGS)
+
+PHONY += bake-db-local
+bake-db-local: DB_BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
+bake-db-local: bake-all-db
