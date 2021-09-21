8b059ae89d4f0eab1c3604914f7059a3aaca15ec
Marko Korhonen <marko.korhonen@druid.fi>
2021-09-21 06:37:47 +0300

    GHA build misc images - load to local

--- misc/build.mk
+++ misc/build.mk
@@ -1,9 +1,14 @@
 BUILD_TARGETS += bake-all-misc
+MISC_BAKE_FLAGS := --pull --push
 
 PHONY += bake-all-misc
 bake-all-misc: ## Bake all misc images
-	@cd misc && ALPINE_VERSION=$(call get_alpine_version) docker buildx bake --pull --push
+	@cd misc && ALPINE_VERSION=$(call get_alpine_version) docker buildx bake $(MISC_BAKE_FLAGS)
 
 PHONY += bake-misc-%
 bake-misc-%:
 	@cd misc && ALPINE_VERSION=$(call get_alpine_version) docker buildx bake --pull --push $*
+
+PHONY += bake-misc-local
+bake-misc-local: DB_BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
+bake-misc-local: bake-all-misc
