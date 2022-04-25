8a483de136d7544648b1a62250ffb1b70666a396
Marko Korhonen <marko.korhonen@druid.fi>
2022-04-25 08:41:58 +0300

    same make target names for php images

--- php/build.mk
+++ php/build.mk
@@ -1,22 +1,22 @@
 BAKE_FLAGS := --pull --no-cache --push
 
-PHONY += bake-all-php
-bake-all-php: ## Bake all PHP images (incl. Drupal)
+PHONY += php-bake-all
+php-bake-all: ## Bake all PHP images
 	@ALPINE_VERSION=$(call get_alpine_version) \
 	PHP74_MINOR=$(call get_php_minor,7.4) PHP80_MINOR=$(call get_php_minor,8.0) PHP81_MINOR=$(call get_php_minor,8.1) \
 		docker buildx bake -f php/docker-bake.hcl $(BAKE_FLAGS)
 
-PHONY += bake-php-print
-bake-php-print: BAKE_FLAGS := --print
-bake-php-print: bake-all-php
+PHONY += php-bake-print
+php-bake-print: BAKE_FLAGS := --print
+php-bake-print: php-bake-all ## Print bake plan for PHP images
 
-PHONY += bake-php-local
-bake-php-local: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
-bake-php-local: bake-all-php run-php-tests ## Bake all PHP images locally
+PHONY += php-bake-local
+php-bake-local: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
+php-bake-local: php-bake-all run-php-tests ## Bake all PHP images locally
 
-PHONY += bake-php-test
-bake-php-test: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH) --set *.target=test
-bake-php-test: bake-all-php
+PHONY += php-bake-test
+php-bake-test: BAKE_FLAGS := --pull --progress plain --no-cache
+php-bake-test: php-bake-all run-php-tests ## CI test for PHP images
 
 PHONY += run-php-tests
 run-php-tests:
