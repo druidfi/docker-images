ad2ae679254b146cb3ce4254f9511d6c10dfbad3
Marko Korhonen <marko.korhonen@druid.fi>
2023-12-08 08:44:09 +0200

    Alpine 3.19

--- php/build.mk
+++ php/build.mk
@@ -13,11 +13,6 @@ PHONY += php-bake-local
 php-bake-local: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
 php-bake-local: php-bake-all run-php-tests ## Bake all PHP images locally
 
-PHONY += php-bake-83
-php-bake-83: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
-php-bake-83: ## Bake all PHP images locally
-	PHP83_MINOR=$(call get_php_minor,8.3) docker buildx bake -f php/docker-bake.hcl $(BAKE_FLAGS) php-83
-
 PHONY += php-bake-test
 php-bake-test: BAKE_FLAGS := --pull --progress plain --no-cache
 php-bake-test: php-bake-all run-php-tests ## CI test for PHP images
@@ -30,8 +25,3 @@ run-php-tests:
 	@docker run --rm -t -v $(CURDIR)/tests/scripts:/app/scripts druidfi/drupal-web:php-8.2 /app/scripts/tests.sh
 	$(call step,Run tests in druidfi/drupal-web:php-8.3)
 	@docker run --rm -t -v $(CURDIR)/tests/scripts:/app/scripts druidfi/drupal-web:php-8.3 /app/scripts/tests.sh
-
-PHONY += run-beta-tests
-run-beta-tests:
-	$(call step,Run tests in druidfi/php-fpm:8.2)
-	@docker run --rm -t -v $(CURDIR)/tests/scripts:/app/scripts druidfi/php-fpm:8.2-beta2 /app/scripts/tests.sh
