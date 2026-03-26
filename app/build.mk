64b8823edfc6e312e9d6a671dd4b5cdcb13dde19
Marko Korhonen <marko.korhonen@druid.fi>
2026-03-26 17:53:52 +0200

    Make build.mk similar to others

--- app/build.mk
+++ app/build.mk
@@ -1,21 +1,24 @@
 BAKE_FLAGS := --pull --no-cache --push
 
-PHONY += app-bake-all
-app-bake-all: ## Bake all PHP images
+PHONY += --app-bake
+--app-bake:
 	@PHP83_MINOR=$(call get_php_fpm_minor,8.3) PHP84_MINOR=$(call get_php_fpm_minor,8.4) PHP85_MINOR=$(call get_php_fpm_minor,8.5) \
 		docker buildx bake -f app/docker-bake.hcl $(BAKE_FLAGS)
 
+PHONY += app-bake-all
+app-bake-all: buildx-create --app-bake buildx-destroy ## Bake all APP images
+
 PHONY += app-bake-print
 app-bake-print: BAKE_FLAGS := --print
-app-bake-print: app-bake-all ## Print bake plan for App images
+app-bake-print: --app-bake ## Print bake plan for App images
 
 PHONY += app-bake-local
 app-bake-local: BAKE_FLAGS := --pull --progress plain --no-cache --load --set *.platform=linux/$(CURRENT_ARCH)
-app-bake-local: app-bake-all run-app-tests ## Bake all App images locally
+app-bake-local: --app-bake run-app-tests ## Bake all App images locally
 
 PHONY += app-bake-test
 app-bake-test: BAKE_FLAGS := --pull --progress plain --no-cache
-app-bake-test: app-bake-all run-app-tests ## CI test for App images
+app-bake-test: --app-bake run-app-tests ## CI test for App images
 
 PHONY += run-app-tests
 run-app-tests:
