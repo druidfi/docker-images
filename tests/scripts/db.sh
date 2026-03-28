3832910f73607627f36fa0b3b7298fc26605a8fa
Marko Korhonen <marko.korhonen@druid.fi>
2026-03-28 09:34:26 +0200

    Add baseline drush.yml

--- tests/scripts/db.sh
+++ tests/scripts/db.sh
@@ -19,3 +19,23 @@ title "Test that DigiCertGlobalRootCA.crt.pem exists"
 if [ ! -f /opt/ssl/DigiCertGlobalRootCA.crt.pem ]; then
   error "DigiCertGlobalRootCA.crt.pem not found at /opt/ssl/"
 fi
+
+title "Test that /etc/drush/drush.yml exists"
+
+if [ ! -f /etc/drush/drush.yml ]; then
+  error "/etc/drush/drush.yml not found"
+fi
+
+title "Test that /etc/drush/drush.yml contains skip-ssl and structure-tables"
+
+if ! grep -q "skip-ssl" /etc/drush/drush.yml; then
+  error "/etc/drush/drush.yml does not contain skip-ssl"
+fi
+
+if ! grep -q "no-tablespaces" /etc/drush/drush.yml; then
+  error "/etc/drush/drush.yml does not contain no-tablespaces"
+fi
+
+if ! grep -q "structure-tables" /etc/drush/drush.yml; then
+  error "/etc/drush/drush.yml does not contain structure-tables"
+fi
