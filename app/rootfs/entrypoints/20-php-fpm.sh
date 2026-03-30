7907bacc86411d18ed9bc03c8cdcfe4d0116c08d
Marko Korhonen <marko.korhonen@druid.fi>
2026-03-30 21:52:53 +0300

    Fix file permissions

--- app/rootfs/entrypoints/20-php-fpm.sh
+++ app/rootfs/entrypoints/20-php-fpm.sh
@@ -2,7 +2,7 @@
 
 echo "Start up PHP-FPM..."
 
-php-fpm &
+doas php-fpm &
 
 # Usage: php [-n] [-e] [-h] [-i] [-m] [-v] [-t] [-p <prefix>] [-g <pid>] [-c <file>] [-d foo[=bar]] [-y <file>] [-D] [-F [-O]]
 #  -c <path>|<file> Look for php.ini file in this directory
